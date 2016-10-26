5f1b161f855e439630da54f5bdfdb1f620cde978
Eric Wolf <eric.wolf123@gmail.com>
2016-10-26 22:01:13 +0800

    Random forests, proper scores, ALU

--- calculate_features.py
+++ calculate_features.py
@@ -1,20 +1,21 @@
-from intervaltree import IntervalTree
-from sklearn import svm
-from sklearn.model_selection import KFold
 from parse import *
 import random
 import itertools
 import time
 import numpy as np
-from sklearn.metrics import roc_auc_score, precision_recall_curve, f1_score
+from pyfasta import Fasta
+from intervaltree import IntervalTree
+import sys
 
 print("Loading files...")
 t0 = time.time()
 
-seqs = read_fasta('data/hg19.fa')
+seqs = Fasta('data/hg19.fa')
 crnas = read_bed('data/hsa_hg19_Rybak2015.bed')
 exons = read_bed('data/all_exons.bed')
+alus = read_bed('data/hg19_Alu.bed')
 not_crnas = read_bed('tmp/negatives.bed')
+not_crnas = not_crnas[:len(crnas)]
 
 t1 = time.time()
 print("%.2fs" % (t1 - t0))
@@ -27,23 +28,56 @@ labels = np.empty([ len(crnas) + len(not_crnas) ], np.int32)
 labels[:len(crnas)] = 1
 labels[len(crnas):] = 0
 
-# Build features
-k = 7
+# Build ALU tree
+alu_trees = {}
+chromosomes = [ 'chr%d' % i for i in range(1, 23) ] + [ 'chrX', 'chrY' ]
+for c in chromosomes:
+	alu_trees[c][c.start:c.end] = True
 
+# Build features
+alu_flank_lengths = [ 50, 100, 200, 500 ]
+
+K = 5
 key_kmers = []
-for i in range(1, k+1):
+for i in range(1, K+1):
 	key_kmers += [ "".join(c) for c in itertools.combinations_with_replacement('ACGT', i) ]
 
-features = np.empty([ len(data), len(key_kmers) ])
-for i, gene in enumerate(data):
+# Subset FIXME
+subset = list(range(0, len(data)))
+random.shuffle(subset)
+subset = subset[0:len(data) // 10]
+
+NUM_FEATURES = len(key_kmers) + 2 * len(alu_flank_lengths) + 1
+features = np.empty([ len(subset), NUM_FEATURES ])
+labels = labels[subset]
+
+for i, j in enumerate(subset):
+	gene = data[j]
 	gene_data = get_gene_data(gene, seqs)
 
 	# kmers
-	kmer_features = [ gene_data.count(kmer) / (gene.end - gene.start) 
-		for kmer in key_kmers ]
+	kmer_features = [ 
+		gene_data.count(kmer) / (gene.end - gene.start) 
+		for kmer in key_kmers 
+	]
 
-	# Add length of gene
-	features[i] = [ gene.end - gene.start ] + kmer_features
+	# ALU counts
+	alu_counts = [
+		len(alu_trees[gene.chr_n][gene.start - length:gene.start])
+		for length in alu_flank_lengths
+	] + [
+		len(alu_trees[gene.chr_n][gene.end:gene.end + length])
+		for length in alu_flank_lengths
+	] + [
+		alu_trees[gene.chr_n][gene.start:gene.end]
+	]
+
+	# Concatenate features
+	features[i] = kmer_features + alu_counts
+
+	if i % 1000 == 0:
+		print('.', end='')
+		sys.stdout.flush()
 
 np.save('tmp/features.npy', features)
 np.save('tmp/labels.npy', labels)
--- validate.py
+++ validate.py
@@ -2,20 +2,52 @@ import time
 import numpy as np
 from sklearn import svm
 from sklearn.model_selection import KFold
-from parse import *
-import random
-import itertools
-import time
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, precision_recall_curve, f1_score
 
-print("Loading files...")
+features = np.load('tmp/features.npy')
+labels = np.load('tmp/labels.npy')
+
+# k Fold
+def test(model, params):
+	print(', '.join('%s: %s' % (k,v) for k, v in params.items()))
+
+	kf = KFold(n_splits=10, shuffle=True)
+
+	scores = []
+	for train, test in kf.split(features):
+		try:
+			model.fit(features[train], labels[train])
+		except KeyboardInterrupt:
+			print("Aborted")
+			return
+		finally:
+			y_true = labels[test]
+			y_pred = clf.predict(features[test])
+
+			roc_auc = roc_auc_score(y_true, y_pred)
+			f1 = f1_score(y_true, y_pred)
+			scores.append([ roc_auc, f1 ])
+
+			print("%.4f\t%.4f" % (roc_auc, f1))
+
+	if len(scores) > 0:
+		print("Mean scores:")
+		n = len(scores)
+		m = len(scores[0])
+		means = [ 
+			sum(s[i] for s in scores) / n
+			for i in range(0, m) 
+		]
+		print(("%.4f\t" * m) % tuple(means))
+
+# Random Forest
+print("Testing RF...")
 t0 = time.time()
 
-seqs = read_fasta('data/hg19.fa')
-crnas = read_bed('data/hsa_hg19_Rybak2015.bed')
-exons = read_bed('data/all_exons.bed')
-kmer_features = np.load('tmp/features.npy')
-labels = np.load('tmp/labels.npy')
+params = { 'n_estimators': 120 }
+clf = RandomForestClassifier(**params)
+test(clf, params)
 
 # SVM
 t1 = time.time()
@@ -23,22 +55,9 @@ print("%.2fs" % (t1 - t0))
 print("Testing SVM...")
 t0 = time.time()
 
-clf = svm.SVC()
-kf = KFold(n_splits=10, shuffle=True)
-
-print("%6s %6s" % ("ROC", "F1"))
-
-for train, test in kf.split(kmer_features):
-	clf.fit(kmer_features[train], labels[train])
-
-	y_true = labels[test]
-	y_pred = clf.predict(kmer_features[test])
-
-	roc_auc = roc_auc_score(y_true, y_pred)
-	f1 = f1_score(y_true, y_pred)
-
-	print("%.4f\t%.4f" % (roc_auc, f1))
+params = { 'kernel': 'rbf' }
+clf = svm.SVC(**params)
+test(clf, params)
 
 t1 = time.time()
 print("%.2fs" % (t1 - t0))
-
