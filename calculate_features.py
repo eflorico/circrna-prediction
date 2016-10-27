af1cd8471fd5e51c3fca37997316ed6a7a34c6e4
Eric Wolf <eric.wolf123@gmail.com>
2016-10-27 23:29:01 +0800

    More ALU flanks; grid search; feature ranking

--- calculate_features.py
+++ calculate_features.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 from pyfasta import Fasta
 from intervaltree import IntervalTree
+from sklearn import sklearn.preprocessing
 import sys
 
 tick("Loading files...")
@@ -39,17 +40,33 @@ tick("Building features...")
 # Build features
 alu_flank_lengths = [ 50, 100, 200, 500 ]
 
-K = 5
+def flanks(gene):
+	flanks = []
+	start, end = gene.start, gene.end
+	for length in alu_flank_lengths:
+		left = start - length
+		right = min(start + length, end)
+		flanks.extend([ ( left, start), (left, right), (start, right) ])
+
+		left = max(end - length, start)
+		right = end + length
+		flanks.extend([ ( left, end), (left, right), (end, right) ])
+	flanks.append((start, end))
+	return flanks
+
+K = 7
 key_kmers = []
 for i in range(1, K+1):
 	key_kmers += [ "".join(c) for c in itertools.combinations_with_replacement('ACGT', i) ]
+
+chromosomes = crnas.keys()
 
 # Subset FIXME
 subset = list(range(0, len(data)))
 random.shuffle(subset)
 subset = subset[0:len(data) // 10]
 
-NUM_FEATURES = len(key_kmers) + 2 * len(alu_flank_lengths) + 1
+NUM_FEATURES = len(key_kmers) + 6 * len(alu_flank_lengths) + 1 + 24
 features = np.empty([ len(subset), NUM_FEATURES ])
 labels = labels[subset]
 
@@ -60,19 +77,24 @@ for i, j in enumerate(subset):
 	# kmers
 	kmer_features = [ 
 		gene_data.count(kmer) / (gene.end - gene.start) 
-		for kmer in key_kmers 
+		for kmer in key_kmers
 	]
 
 	# ALU counts
-	alu_counts = [
-		len(alu_trees[gene.chr_n][gene.start - length:gene.start])
-		for length in alu_flank_lengths
-	] + [
-		len(alu_trees[gene.chr_n][gene.end:gene.end + length])
-		for length in alu_flank_lengths
-	] + [
-		len(alu_trees[gene.chr_n][gene.start:gene.end])
-	]
+	alu_counts = []
+	for start, end in flanks(gene):
+		matching_alus = alu_trees[gene.chr_n][start:end]
+		if len(matching_alus) > 0:
+			alu = list(matching_alus)[0]
+			alu_counts.append((min(end, alu.end) - max(start, alu.begin)) / (end - start))
+		else:
+			alu_counts.append(0.)
+
+	# alu_counts = [
+	# 	#min(len(alu_trees[gene.chr_n][start:end]), 1) 0/1-score
+	# 	for start, end in flanks(gene)
+		
+	# ]
 
 	# Concatenate features
 	features[i] = kmer_features + alu_counts
@@ -80,6 +102,8 @@ for i, j in enumerate(subset):
 	if i % 1000 == 0:
 		print('.', end='')
 		sys.stdout.flush()
+
+features 
 
 np.save('tmp/features.npy', features)
 np.save('tmp/labels.npy', labels)
--- validate.py
+++ validate.py
@@ -1,16 +1,28 @@
 import time
+import itertools
+import random
 import numpy as np
+from util import tick
 from sklearn import svm
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import roc_auc_score, precision_recall_curve, f1_score
 
 features = np.load('tmp/features.npy')
 labels = np.load('tmp/labels.npy')
 
+# Zero-center
+features = (features - features.mean(axis=0))
+
+# Subset for grid search
+subset = list(range(0, len(features)))
+subset = random.sample(subset, 1000)
+
 # k Fold
-def test(model, params):
-	print(', '.join('%s: %s' % (k,v) for k, v in params.items()))
+def test(model):
+	print(', '.join('%s: %s' % (k,v) for k, v in model.get_params().items()))
 
 	kf = KFold(n_splits=10, shuffle=True)
 
@@ -18,10 +30,7 @@ def test(model, params):
 	for train, test in kf.split(features):
 		try:
 			model.fit(features[train], labels[train])
-		except KeyboardInterrupt:
-			print("Aborted")
-			return
-		finally:
+		
 			y_true = labels[test]
 			y_pred = clf.predict(features[test])
 
@@ -30,6 +39,9 @@ def test(model, params):
 			scores.append([ roc_auc, f1 ])
 
 			print("%.4f\t%.4f" % (roc_auc, f1))
+		except KeyboardInterrupt:
+			print("Aborted")
+			return
 
 	if len(scores) > 0:
 		print("Mean scores:")
@@ -41,23 +53,71 @@ def test(model, params):
 		]
 		print(("%.4f\t" * m) % tuple(means))
 
-# Random Forest
-print("Testing RF...")
-t0 = time.time()
-
+tick("Random Forest...")
 params = { 'n_estimators': 120 }
 clf = RandomForestClassifier(**params)
-test(clf, params)
+#test(clf)
 
-# SVM
-t1 = time.time()
-print("%.2fs" % (t1 - t0))
-print("Testing SVM...")
-t0 = time.time()
+tick("Feature ranking...")
 
+# Fields
+alu_flank_lengths = [ 50, 100, 200, 500 ]
+K = 7
+
+# kmer field names
+feature_names = []
+for i in range(1, K+1):
+	feature_names += [ "".join(c) for c in itertools.combinations_with_replacement('ACGT', i) ]
+
+for length in alu_flank_lengths:
+	feature_names.extend([
+		'-%d/0 left ALUs' % length,
+		'-%d/+%d left ALUs' % (length, length),
+		'0/+%d left ALUs' % length,
+		'-%d/0 right ALUs' % length,
+		'-%d/+%d right ALUs' % (length, length),
+		'0/+%d right ALUs' % length
+	])
+
+clf.fit(features, labels)
+ranking = sorted(zip(clf.feature_importances_, feature_names), reverse=True)
+for rank, name in ranking:
+	print("%.4f %s" % (rank, name))
+
+
+tick("Lin Reg...")
+params = { 'n_jobs': -1, 'C': 10000 }
+param_grid = { 'n_jobs': [-1], 'C': [ 10 ** i for i in range(4, 10)] }
+clf = GridSearchCV(LogisticRegression(), param_grid)
+clf.fit(features[subset], labels[subset])
+print(clf.best_estimator_)
+test(clf.best_estimator_)
+
+# tick("kNN")
+# clf = KNeighborsClassifier(n_jobs=-1)
+# test(clf)
+
+tick("SVM grid search...")
+params = { 'C': 2 ** 6, 'gamma': 2 ** -9, 'kernel': 'rbf' }
+param_grid = [
+#	{ 'kernel':['rbf']}
+   {'C': [10**i for i in range(3,8)], 'kernel': ['linear']},
+   { 'C': [2 ** 6], 'gamma': [2 ** -9], 'kernel': ['rbf'] }
+#  { 'C': [ 10 ** i for i in range(4, 9) ], 
+#  	'gamma': [ 2 ** i for i in range(-10, -4) ], 
+#  	'kernel': ['rbf']
+#  },
+# {'C': [1000, 10000], 'degree': [3, 4], 'kernel': ['poly']},
+ ]
+
+#train, test = KFold(n_splits=100, shuffle=True)
+
+clf = GridSearchCV(svm.SVC(), param_grid)
+clf.fit(features[subset], labels[subset])
+print(clf.best_estimator_)
+
+tick("SVM...")
 params = { 'kernel': 'rbf' }
-clf = svm.SVC(**params)
-test(clf, params)
+test(clf.best_estimator_)
 
-t1 = time.time()
-print("%.2fs" % (t1 - t0))
+tick()
